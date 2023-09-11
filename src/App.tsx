40e50fef4e0150ffacd713703bd137489152e351
MohammedNader <business.naderghu@gmail.com>
2023-09-11 19:49:13 +0300

    adding the strucure of the project and the custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMultiStepForm } from "./hooks/useMultiStepForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultiStepForm([<div>One</div>, <div>two</div>]);
 
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="relative bg-[#1d1d1d] border-[1px] border-white p-[32px] m-2 rounded-md w-[400px]">
+      <form onSubmit={onSubmit}>
+        <div className=" absolute top-3 right-3">
+          {currentStepIndex + 1}/{steps.length}
+        </div>
+        {step}
+        <div className="flex justify-end gap-3">
+          {!isFirstStep && (
+            <button className="btn" onClick={back}>
+              back
+            </button>
+          )}
+          <button className="btn" onClick={next}>
+            {isLastStep ? "Finish" : "Next"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
